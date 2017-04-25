887a74c1a57578b21bf210101f0eba594775d52a
Danielle Carrick <dacarrick@gmail.com>
2017-04-25 16:11:57 +0300

    TEST

--- public/js/services/apiFactory.js
+++ public/js/services/apiFactory.js
@@ -35,6 +35,16 @@ var apiFactory = {};
             }, function(err) {});
     };
 
+    apiFactory.TESTFUNCTION = function(location) {
+        return $http.get('http://api.openweathermap.org/data/2.5/weather?',
+          { params: { q: location,
+            appid: 'b51ff059850fb59ef5b5085a6e089a74' } })
+            .then(function(response) {
+                console.log(response.data);
+                return response.data
+            }, function(err) {});
+    };
+
 return apiFactory;
 
 });
