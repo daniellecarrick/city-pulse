9ef3f2b0a24c811ddea8cb88d7ed8ec5f8c942ca
Danielle Carrick <dacarrick@gmail.com>
2017-04-25 16:17:35 +0300

    deleted comments

--- public/js/services/apiFactory.js
+++ public/js/services/apiFactory.js
@@ -1,26 +1,4 @@
 app.service('apiFactory', function($http) {
-  //$http.get('http://api.openweathermap.org/data/2.5/weather?q=', config).then(successCallback, errorCallback);
-  // Simple GET request example:
-/*$http({
-  method: 'GET',
-  url: 'http://api.openweathermap.org/data/2.5/weather?',
-  params: {q: $scope.location}
-}).then(function successCallback(response) {
-    console.log(response);
-  }, function errorCallback(err) {
-    console.log(err);
-  });*/
-
-
-/*$http({
-  method: 'GET',
-  url: 'http://api.openweathermap.org/data/2.5/weather?q=London',
-  params: { appid: 'b51ff059850fb59ef5b5085a6e089a74'}
-}).then(function successCallback(response) {
-    console.log(response);
-  }, function errorCallback(err) {
-    console.log(err);
-  });*/
 
 var apiFactory = {};
 
