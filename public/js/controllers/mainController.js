b5906c6c9e6ca31f40143be31081a4dc3afe00ed
Danielle Carrick <dacarrick@gmail.com>
2017-04-25 15:38:01 +0300

    location is being saved in controller

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,3 +1,11 @@
 app.controller('mainController', function($scope, apiFactory) {
+
   $scope.location;
+
+  //search triggered on button click
+  $scope.search = function(location) {
+    $scope.location = location;
+    console.log(location);
+
+  }
 });
