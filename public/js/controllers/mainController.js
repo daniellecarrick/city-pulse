d0545c4edfd148c881e64d79b005ef43092758aa
Danielle Carrick <dacarrick@gmail.com>
2017-05-03 22:36:37 +0300

    installed masonry and prettified the code

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,48 +1,48 @@
 app.controller('mainController', function($scope, apiFactory, $http) {
 
-  $scope.city = 'New York';
-  $scope.coord = {};
+    $scope.city = 'New York';
+    $scope.coord = {};
 
-  //search triggered on button click
-  $scope.search = function(city) {
-    console.log(city);
-    // give the searched city to our friend getCityInfo in the factory
-    apiFactory.getCityInfo(city).then(function (allData) {
-      console.log(allData);
-      // data is returned as an array of objects - one for each api database
-      $scope.fourDB = allData[0].groups[0].items;
-      $scope.photoDB = allData[1].photos.photo;
-      $scope.weatherDB = allData[2];
-      $scope.tweets = allData[3];
-      $scope.trends = allData[4][0].trends;
+    //search triggered on button click
+    $scope.search = function(city) {
+            console.log(city);
+            // give the searched city to our friend getCityInfo in the factory
+            apiFactory.getCityInfo(city).then(function(allData) {
+                console.log(allData);
+                // data is returned as an array of objects - one for each api database
+                $scope.fourDB = allData[0].groups[0].items;
+                $scope.photoDB = allData[1].photos.photo;
+                $scope.weatherDB = allData[2];
+                $scope.tweets = allData[3];
+                $scope.trends = allData[4][0].trends;
 
-      setBackground($scope.weatherDB);
-    });
+                setBackground($scope.weatherDB);
+            });
 
-  } //end of search() function
+        } //end of search() function
 
-  /* Background color is based on temperature */
-  var setBackground = function(weatherDB) {
-    var temp = weatherDB.main.temp;
-    var colorScale = d3.scaleLinear().domain([0, 50, 100]).range(['#1e3c72', '#6190E8', '#DC2424']);
-    /*var bgColor = d3.interpolateRdYlBu(colorScale(temp));
-    var bgColor1 = d3.interpolateRdYlBu(colorScale(temp+20));*/
-    var bgColor = colorScale(temp);
-    var bgColor1 = colorScale(temp+10);
-    d3.select('.top-section').style('background', 'linear-gradient(to bottom,'+ bgColor + ',' + bgColor1);
+    /* Background color is based on temperature */
+    var setBackground = function(weatherDB) {
+        var temp = weatherDB.main.temp;
+        var colorScale = d3.scaleLinear().domain([0, 50, 100]).range(['#1e3c72', '#6190E8', '#DC2424']);
+        /*var bgColor = d3.interpolateRdYlBu(colorScale(temp));
+        var bgColor1 = d3.interpolateRdYlBu(colorScale(temp+20));*/
+        var bgColor = colorScale(temp);
+        var bgColor1 = colorScale(temp + 10);
+        d3.select('.top-section').style('background', 'linear-gradient(to bottom,' + bgColor + ',' + bgColor1);
     }
 
-//* Tweet stuff */
-  $scope.tweets=[]
-  $scope.search($scope.city);
+    //* Tweet stuff */
+    $scope.tweets = []
+    $scope.search($scope.city);
 
-//* Change from F to C and back */
-/*  $scope.fahrenheit = true;
-  $scope.tempFahrenheit = $scope.weatherDB.main.temp;
+    //* Change from F to C and back */
+    /*  $scope.fahrenheit = true;
+      $scope.tempFahrenheit = $scope.weatherDB.main.temp;
 
-  $scope.switchTemperature = function () {
-    $scope.fahrenheit = !$scope.fahrenheit;
-    $scope.tempCelsius = Math.round((($scope.weatherDB.main.temp) - 32) / 1.8);
-  }*/
+      $scope.switchTemperature = function () {
+        $scope.fahrenheit = !$scope.fahrenheit;
+        $scope.tempCelsius = Math.round((($scope.weatherDB.main.temp) - 32) / 1.8);
+      }*/
 
 });
--- public/js/services/apiFactory.js
+++ public/js/services/apiFactory.js
@@ -1,18 +1,18 @@
 app.service('apiFactory', function($http) {
 
-var apiFactory = {};
+    var apiFactory = {};
 
     // Getting city data from our city route on the server
     apiFactory.getCityInfo = function(city) {
-      return $http.get('/city/'+city ) // let's go to the server
-        .then(function(response) {
-            console.log(response.data);
-            return response.data
-        }, function(err) {
-          console.log(err);
-        });
+        return $http.get('/city/' + city) // let's go to the server
+            .then(function(response) {
+                console.log(response.data);
+                return response.data
+            }, function(err) {
+                console.log(err);
+            });
     };
 
-return apiFactory;
+    return apiFactory;
 
 });
