3efea0a23c09b103fb084b141769780d3080b8d8
nate chesley <nate@almanac.local>
2017-05-04 18:11:20 +0300

    a skosh more carousel progress

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,1 @@
 var app = angular.module('cityPulse', ['ngAutocomplete', 'ui.bootstrap']);
-
-/*app.config(function($sceDelegateProvider, $sceProvider) {
-  $sceDelegateProvider.resourceUrlWhitelist([
-    // Allow same origin resource loads.
-    'self',
-    // Allow loading from our assets domain.  Notice the difference between * and **.
-    'https://maps.googleapis.com/**'
-  ]);
-  $sceProvider.enabled(false);
-});*/
-
